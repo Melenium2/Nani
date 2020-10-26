6afd63dc26e66c2e513f30907acc5643b36e30ee
Melenium2 <osumelenium@gmail.com>
2020-10-26 18:14:26 +0500

    Added cache

---
+++ internal/app/cache/cache.go
@@ -0,0 +1,116 @@
+package cache
+
+import (
+	"Nani/internal/app/file"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"path"
+	"sync"
+	"syscall"
+)
+
+var debug bool = false
+
+type Storage interface {
+	Set(key string, value interface{})
+	GetV(key string) (interface{}, error)
+}
+
+type Item struct {
+	V       interface{} `json:"V,omitempty"`
+	Expired int64       `json:"Expired,omitempty"`
+}
+
+type Cache struct {
+	Clear     bool
+	Debug     bool
+	cachename string
+	store     map[string]Item
+	mutex     sync.Mutex
+}
+
+func (c *Cache) Set(key string, value interface{}) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	c.store[key] = Item{value, 0}
+}
+
+func (c *Cache) GetV(key string) (interface{}, error) {
+	v, ok := c.store[key]
+	if !ok {
+		return nil, errors.New(fmt.Sprintf("value with key %s not found", key))
+	}
+
+	return v.V, nil
+}
+
+func (c *Cache) dump() {
+	if debug {
+		log.Print("start dump")
+	}
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		log.Printf("creating cache dump coz %s", <-sig)
+		if debug {
+			log.Print("sig complete")
+		}
+
+		close(sig)
+		if len(c.store) > 0 {
+			f := file.New(c.cachename)
+			str, err := json.Marshal(c.store)
+			if err != nil {
+				panic(err)
+			}
+			f.WriteLines(string(str))
+
+			log.Print("cache crated")
+		} else {
+			log.Print("skip creating")
+
+			return
+		}
+	}()
+}
+
+func (c *Cache) load() {
+	if c.Clear {
+		err := os.Remove(c.cachename)
+		if err != nil {
+			log.Printf("Can not remove file %s. File not found", c.cachename)
+		}
+	} else {
+		f := file.New(c.cachename)
+		b, err := f.ReadAll()
+		if err != nil {
+			panic(err)
+		}
+
+		err = json.Unmarshal(b, &c.store)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func New(new bool, cachefile ...string) *Cache {
+	filename := path.Join("./cache.json")
+	if len(cachefile) > 0 {
+		filename = cachefile[0]
+	}
+
+	c := &Cache{
+		cachename: filename,
+		store:     make(map[string]Item),
+		Clear:     new,
+	}
+	c.load()
+	c.dump()
+	return c
+}
---
+++ internal/app/cache/cache_test.go
@@ -0,0 +1,69 @@
+package cache_test
+
+import (
+	"Nani/internal/app/cache"
+	"encoding/json"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"os"
+	"testing"
+)
+
+func TestSetV_ShouldStoreToCacheNewValue_NoErrors(t *testing.T) {
+	k, v := "key", "value"
+	c := cache.New(true)
+	c.Set(k, v)
+	res, err := c.GetV(k)
+	assert.NoError(t, err)
+	assert.NotNil(t, res)
+	assert.Equal(t, v, res.(string))
+}
+
+func TestGetV_ShouldReturnValueFromCache_NoErrors(t *testing.T) {
+	k, v := "key", "value"
+	c := cache.New(true)
+	c.Set(k, v)
+	res, err := c.GetV(k)
+	assert.NoError(t, err)
+	assert.NotNil(t, res)
+	assert.Equal(t, v, res.(string))
+}
+
+func TestGetV_ShouldReturnErrorCozKeyIsInvalid_Error(t *testing.T) {
+	k, v := "key", "value"
+	c := cache.New(true)
+	c.Set(k, v)
+	res, err := c.GetV(k + "1")
+	assert.Error(t, err)
+	assert.Nil(t, res)
+}
+
+func TestLoad_ShouldStoreAllJsonToCache_NoError(t *testing.T) {
+	_, err := ioutil.ReadFile("./cache.json")
+	if err != nil {
+		t.Skipf("file /cache.json not found %s", err)
+	}
+
+	c := cache.New(false)
+	v, err := c.GetV("key")
+	assert.NoError(t, err)
+	assert.NotNil(t, v)
+	assert.Equal(t, "value", v.(string))
+}
+
+func TestLoad_ShouldThrowPanicCozFileNotFound_Error(t *testing.T) {
+	assert.Panics(t, func() {
+		cache.New(false)
+	})
+}
+
+func TestLoad_ShouldThrowPanicCozInalidJosn_Erro(t *testing.T) {
+	assert.Panics(t, func() {
+		j, _ := json.Marshal(`{"123" "1321323}`)
+		ioutil.WriteFile("./cache.json", j, 0644)
+		cache.New(false)
+	})
+	os.Remove("./cache.json")
+}
+
+
